0ab3cf26fd07fc0ca3c2cfe63686a9d84bb5b433
mairarafiq301 <mairarafiq301@gmail.com>
2024-03-17 04:51:00 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer" ;
+
+let answers = await inquirer.prompt({
+    name : "age",
+    type: "number" ,
+    message: "Enter your age:"
+
+});
+console.log(" Inshallah , after " +  ( 60 - answers.age )  + " years you will be 60 years old. ");
+
+
+
+
+
+
+
+
